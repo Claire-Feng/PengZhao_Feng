b24160f5ea96d0b8081d8ffc963699e32c1a0c25
Claire Feng <cf@ClairedeMBP.attlocal.net>
2019-09-18 16:48:04 -0500

    commit

--- Assignments/home_work_assignment_4/working_with_lists_2.py
+++ Assignments/home_work_assignment_4/working_with_lists_2.py
@@ -5,7 +5,9 @@ Due Date: 09/18/2019
 Class: MSITM 6341
 '''
 
+#generate a list of odd numbers from 3 to 50
 odd_numbers = list(range(3,50,2))
 print(odd_numbers)
+#the first 5 odd numbers in the list using a for loop, with each number printed on a single line
 for odd_number in odd_numbers[:5]:
        print(odd_number)
