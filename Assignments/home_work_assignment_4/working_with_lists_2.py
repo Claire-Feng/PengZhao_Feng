61864fb963b3556ebf5c4cd0a2567d59542850e9
Claire Feng <cf@ClairedeMBP.attlocal.net>
2019-09-18 16:41:26 -0500

    commit

--- Assignments/home_work_assignment_4/working_with_lists_2.py
+++ Assignments/home_work_assignment_4/working_with_lists_2.py
@@ -5,7 +5,7 @@ Due Date: 09/18/2019
 Class: MSITM 6341
 '''
 
-odd_numbers = list(range(3,50,3))
+odd_numbers = list(range(3,50,2))
 print(odd_numbers)
 for odd_number in odd_numbers[:5]:
        print(odd_number)
