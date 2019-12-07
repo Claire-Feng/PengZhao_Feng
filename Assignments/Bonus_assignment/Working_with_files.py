210f3ebbd60046bb692837261470c15be2c18e9b
Claire Feng <cf@ClairedeMacBook-Pro.local>
2019-12-07 00:40:10 -0600

    master

---
+++ Assignments/Bonus_assignment/Working_with_files.py
@@ -0,0 +1,25 @@
+import re
+
+fileName = 'test.txt'
+
+# open a file with name 'test.txt'
+with open(fileName, 'w+') as f:
+    #  read()
+    s = f.read()
+    # equals s with None || "" ,this option
+    if str(s) is "" or s is None:
+        print('No store information found.')
+        strInput = input('Enter the email address you would like to store?')
+        # match()
+        if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", strInput):
+            print('Email address is Right!')
+            with open(fileName, 'w') as fw:
+                fw.write(strInput)
+            print('Information saved.')
+        else:
+            print('Please reset your right Email address!')
+            print('Information was not saved.')
+    else:
+        print("Email address loaded: " + s)
+# close your file and end function
+f.close()
